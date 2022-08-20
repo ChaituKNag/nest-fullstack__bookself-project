b682ce63510a1fd30a3edbc1e0e0aec8bd394cf0
Naga Chaitanya Konada <knc.dev@outlook.com>
2022-08-20 12:13:01 +0530

    maint: folder move

---
+++ packages/client-cra/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { httpGet } from "../services/api-service";
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const _books = await httpGet("/api/books");
+      console.log("books list", _books);
+    })();
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
