9b7cc9db96fa3ce0d24d73a5118633433d075695
Naga Chaitanya Konada <knc.dev@outlook.com>
2022-08-27 13:05:15 +0530

    feat: formik and yup packages added

--- packages/client/pages/login.tsx
+++ packages/client/pages/login.tsx
@@ -1,29 +1,51 @@
 import { getCookie } from "cookies-next";
+import { useFormik } from "formik";
 import type { NextPage, NextPageContext } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
 import { httpGet, httpPost } from "../services/api-service";
+import * as Yup from "yup";
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string()
+    .required("Please give a username")
+    .min(5, "Too short")
+    .max(15, "Too long"),
+  password: Yup.string()
+    .required("Please give the password")
+    .min(5, "Too short")
+    .max(16, "Too long")
+});
 
 const Login: NextPage = () => {
   const router = useRouter();
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    const data = await httpPost(
-      `${process.env.NEXT_PUBLIC_WEB_HOST}/api/login`,
-      {
-        username,
-        password
-      }
-    );
+  const { handleSubmit, handleBlur, handleChange, values, errors, touched } =
+    useFormik({
+      initialValues: {
+        username: "",
+        password: ""
+      },
+      validationSchema: LoginSchema,
+      onSubmit: async (values) => {
+        const { username, password } = values;
+        const data = await httpPost(
+          `${process.env.NEXT_PUBLIC_WEB_HOST}/api/login`,
+          {
+            username,
+            password
+          }
+        );
 
-    if (data.status === "success") {
-      router.push("/");
-    }
-  };
+        if (data.status === "success") {
+          router.push("/");
+        }
+      }
+    });
+
+  console.log(errors, touched);
+
   return (
     <div>
       <Head>
@@ -32,30 +54,45 @@ const Login: NextPage = () => {
       <form onSubmit={handleSubmit}>
         <div className="max-w-lg mx-auto flex flex-col">
           <h2 className="text-center py-5 text-2xl font-semibold">Login</h2>
-          <label className="block text-sm" htmlFor="username">
-            Username
+          <label className="block relative mb-5" htmlFor="username">
+            <span className="block text-sm">Username</span>
+            <input
+              className="w-full px-2 py-3 border mb-5 focus:border-yellow-500 border-yellow-600 rounded outline-0 focus:bg-orange-50"
+              id="username"
+              type="text"
+              placeholder="enter your username"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username}
+              required
+            />
+            {errors.username && touched.username && (
+              <p className="text-red-500 text-sm absolute inset-x-0 bottom-0">
+                {errors.username}
+              </p>
+            )}
           </label>
-          <input
-            className="block mb-5 px-2 py-3 border focus:border-yellow-500 border-yellow-600 rounded outline-0 focus:bg-orange-50"
-            id="username"
-            type="text"
-            placeholder="enter your username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label className="block text-sm" htmlFor="password">
-            Password
+          <label className="block relative mb-3" htmlFor="password">
+            <span className="block text-sm">Password</span>
+
+            <input
+              className="w-full px-2 py-3 border mb-5 focus:border-yellow-500 border-yellow-600 rounded outline-0 focus:bg-orange-50"
+              id="password"
+              type="password"
+              placeholder="****"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              required
+            />
+            <p className="text-red-500 text-sm absolute inset-x-0 bottom-0">
+              {errors.password && touched.password && errors.password}
+            </p>
           </label>
-          <input
-            className="block mb-5 px-2 py-3 border focus:border-yellow-500 border-yellow-600 rounded outline-0 focus:bg-orange-50"
-            id="password"
-            type="password"
-            placeholder="****"
-            onChange={(e) => setPassword(e.target.value)}
-          />
           <button
             type="submit"
             className="self-end border border-green-700 px-2 py-1 rounded bg-green-200 hover:bg-green-300 font-semibold disabled:opacity-60 disabled:hover:bg-green-200"
-            disabled={!username || !password}
+            disabled={!!errors.username || !!errors.password}
           >
             Submit
           </button>
--- packages/client/pages/signup.tsx
+++ packages/client/pages/signup.tsx
@@ -1,13 +1,70 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
 
-const Signup = () => {
+const Signup: NextPage = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const handleSubmit = () => {
+    console.log(username, password, confirmPassword);
+  };
   return (
     <div>
       <Head>
         <title>Signup</title>
       </Head>
-      Signup
+      <form onSubmit={handleSubmit}>
+        <div className="max-w-lg mx-auto flex flex-col">
+          <h2 className="text-center py-5 text-2xl font-semibold">Signup</h2>
+          <label className="block text-sm" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="block mb-5 px-2 py-3 border focus:border-yellow-500 border-yellow-600 rounded outline-0 focus:bg-orange-50"
+            id="username"
+            type="text"
+            placeholder="enter your username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label className="block text-sm" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="block mb-5 px-2 py-3 border focus:border-yellow-500 border-yellow-600 rounded outline-0 focus:bg-orange-50"
+            id="password"
+            type="password"
+            placeholder="****"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label className="block text-sm" htmlFor="confirm-password">
+            Confirm password
+          </label>
+          <input
+            className="block mb-5 px-2 py-3 border focus:border-yellow-500 border-yellow-600 rounded outline-0 focus:bg-orange-50"
+            id="confirm-password"
+            type="password"
+            placeholder="****"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="self-end border border-green-700 px-2 py-1 rounded bg-green-200 hover:bg-green-300 font-semibold disabled:opacity-60 disabled:hover:bg-green-200"
+            disabled={!username || !password || !confirmPassword}
+          >
+            Submit
+          </button>
+          <p className="text-sm self-end mt-3">
+            Already have an account?{" "}
+            <Link href="/signup">
+              <a className="font-semibold text-blue-600 hover:text-blue-800">
+                login here
+              </a>
+            </Link>
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
